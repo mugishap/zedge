b4083e1a0d71133215b19a8bf3912cb9dd635496
mugishap <precieuxmugisha@gmail.com>
2022-08-19 18:15:07 +0200

    [feat]:Added basic components

---
+++ resources/views/components/tabs.blade.php
@@ -0,0 +1,7 @@
+<script src="https://cdn.tailwindcss.com"></script>
+<div class="w-screen h-16 bg-white flex items-center justify-center">
+<div class="text-lg text-slate-700 h-full flex items-center justify-center border-b-4 border-b-purple-500 px-3 text-center mx-2">All</div>
+<div class="text-lg text-slate-700 h-full flex items-center justify-center border-b-4 border-b-purple-500 px-3 text-center mx-2">Ringtones</div>
+<div class="text-lg text-slate-700 h-full flex items-center justify-center border-b-4 border-b-purple-500 px-3 text-center mx-2">Wallpapers</div>
+<div class="text-lg text-slate-700 h-full flex items-center justify-center border-b-4 border-b-purple-500 px-3 text-center mx-2">Live Wallpapers</div>
+</div>
---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/all', function () {
+    return view('All',['page' => 'all']);
+});
+
+Route::get('/wallpapers', function () {
+    return view('wallpapers',['page' => 'wallpapers']);
+});
+
+Route::get('/user', function () {
+    return view('user',['page' => 'user']);
+});
+
+
+Route::get('/live-wallpapers', function () {
+    return view('live-wallpapers',['page' => 'live-wallpapers']);
+});
+
+
+Route::get('/ringtones', function () {
+    return view('ringtones',['page' => 'ringtones']);
+});
